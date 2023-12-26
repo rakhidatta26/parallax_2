d0c9aed4c529418daa5d225e4f208780f3fc6d0b
Rakhi Datta <146545970+rakhidatta26@users.noreply.github.com>
2023-12-26 19:10:46 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	 $(window).enllax();
+            
+           $('#some-id').enllax();
+         
+           $('selector').enllax({
+               type: 'background',
+               ratio: 0.5,
+               direction: 'vertical'
+         });
+      
+});
+
